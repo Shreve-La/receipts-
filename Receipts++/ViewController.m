bf3509da1aa816ff8fe1f012383f533fcbf40b9e
Shreve-La <shrevela@gmail.com>
2017-07-20 12:56:10 -0400

    Initial Commit

---
+++ Receipts++/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Receipts++
+//
+//  Created by swcl on 2017-07-20.
+//  Copyright © 2017 Shreve.ca. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
